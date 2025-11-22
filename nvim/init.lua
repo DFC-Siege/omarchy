e6184d862247b93b512c1db11a31154d1c019ee4
Daniel da Silva <dsilvaba@pm.me>
2025-11-22 23:32:01 +0100

    Added cava, fix for copy to clipboard nvim and increased scroll speed in ghostty

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.keymap.set({ "v", "x" }, "<leader>y", [["+y]], {
+  desc = "Yank to system clipboard (No motion)",
+  noremap = true,
+})
