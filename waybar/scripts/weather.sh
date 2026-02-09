8eac796f23b77b85b98dfe5ef955c7eea1fd1ca1
Daniel da Silva <daniel@doetdingen.nl>
2026-02-09 10:45:23 +0100

    Changed weather script

--- waybar/scripts/weather.sh
+++ waybar/scripts/weather.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+cache_file="/tmp/waybar-weather-cache"
 location="zaltbommel"
 
-weather=$(curl -s "wttr.in/${location}?format=%c%t")
+weather=$(curl -sf "wttr.in/${location}?format=%c%t")
 
-if [ $? -eq 0 ] && [ -n "$weather" ]; then
-    echo "{\"text\":\"$weather\", \"tooltip\":\"Weather in $location\"}"
+if [[ $? -eq 0 && -n "$weather" ]]; then
+        echo "$weather" >"$cache_file"
 else
-    echo "{\"text\":\"\", \"tooltip\":\"Weather unavailable\"}"
+        if [[ -f "$cache_file" ]]; then
+                weather=$(cat "$cache_file")
+        else
+                weather="N/A"
+        fi
 fi
+
+echo "{\"text\":\"$weather\", \"tooltip\":\"Weather in $location\"}"
