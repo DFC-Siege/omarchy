b4d7d08ce07fe387d7639d30b34a36ea77f8b95f
Daniel da Silva <daniel@doetdingen.nl>
2026-02-27 15:16:37 +0100

    Added diffview

---
+++ nvim/lua/plugins/diffview.lua
@@ -0,0 +1,8 @@
+return {
+  "sindrets/diffview.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+  keys = {
+    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview" },
+  },
+}
