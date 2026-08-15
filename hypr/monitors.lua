26104de48050ee44e0392ba227fe037b10666f25
Daniel da Silva <dsilvaba@pm.me>
2026-08-15 02:14:39 +0200

    Fixes

--- hypr/monitors.lua
+++ hypr/monitors.lua
@@ -12,4 +12,4 @@ hl.monitor({
 })
 hl.monitor({ output = "desc:ASUSTek COMPUTER INC VG27A", mode = "2560x1440@165", position = "0x0", scale = 1 })
 hl.monitor({ output = "desc:BNQ BenQ GL2250H", mode = "1920x1080@60", position = "2560x360", scale = 1 })
-hl.monitor({ output = "", mode = "preferred", position = "auto", scale = 1 })
+hl.monitor({ output = "", mode = "preferred", position = "auto", scale = 1.25 })
